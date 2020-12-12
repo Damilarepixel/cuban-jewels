07918b7b6a533c36eb23b545bab5aa9620a63d6e
Dreyy00 <padigun0@gmail.com>
2020-12-12 01:00:52 +0100

    router fixes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import mainNav from './components/mainNav'
+import Faq from './pages/Faq'
+import Support from './pages/Support'
+import About from './pages/About'
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <hi>Hello bitch!!</hi>
-    </div>
+    <Router>
+      <div className="App">
+        <mainNav />
+        <Route path="/faq" component={Faq}/>
+        <Route path="/support" component={Support}/>
+        <Route path="/about" component={About}/>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/mainNav.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+
+
+
+function mainNav() {
+    return (
+        <nav>
+            <div>
+                <h4>Mon - Fri 9am - 6pm GMT</h4>
+            </div>
+
+            <div>
+                <h4>20% DISCOUNT ON ALL ITEMS</h4>
+            </div>
+        </nav>
+    )
+}
+
+export default mainNav
+
