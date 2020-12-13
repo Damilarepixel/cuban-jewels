5d3d8a573d137d3ba80bef3e18704ff1ddc76a54
Dreyy00 <padigun0@gmail.com>
2020-12-13 08:22:53 +0100

    styled component fixes

---
+++ src/components/Button.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const Cta = styled.button`
+  background: #0A1826;
+  padding: 3rem 4rem;
+  color: #fff;
+  border-radius: none;
+`
+
+function Button() {
+    return (
+        <Cta>
+            
+        </Cta>
+    )
+}
+
+export default Button
