224a6512691e3e12c240a9fb12d87dbc9938ed9c
Dreyy00 <padigun0@gmail.com>
2020-12-31 08:57:56 +0100

    nav changes

--- src/components/subNav.js
+++ src/components/subNav.js
@@ -6,54 +6,86 @@ import Search from '../Images/search.svg'
 
 
 
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/* Remove menu */
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
 
 
 
 const NavBar = (props) => {
-    const openNav = () => {
-      document.getElementById("menu").classList.toggle("open");
-    };
+    
     return (
       <nav className="">
         <div className="container menuContainer">
           <div className="centered">
             <div className="flex">
-              <h3>Ludigi</h3>
-              <h3 onClick={openNav} className="fa fa-bars menuIcon"></h3>
+              <h3>CUBAN</h3>
+              
             </div>
           </div>
           <div id="menu" className="homeLinks centered">
             <div>
-              <h1 title="Close Menu" onClick={openNav} className="closeBtn">
+              <h1 title="Close Menu" onClick={showMenu} className="closeBtn">
                 x
               </h1>
-              <Link className={props.active === "home" && "accent"} to="/">
+              <Link className={props.active === "home"} to="/">
                 Home
               </Link>
-              <Link className={props.active === "about" && "accent"} to="/about">
-                About
+              <Link className={props.active === "about"} to="/about">
+                Watches
               </Link>
               <Link
-                className={props.active === "services" && "accent"}
+                className={props.active === "services"}
                 to="/services"
               >
-                Services
+                Necklaces & Chains
               </Link>
               <Link
-                className={props.active === "properties" && "accent"}
+                className={props.active === "properties"}
                 to="/properties"
               >
-                Properties
+                Rings
               </Link>
-              <Link
-                className={props.active === "contact" && "accent"}
+              
+            </div>
+            <div class="nav__toggle" id="nav-toggle">
+                    <i class="bx bx-menu"></i>
+                </div>
+          </div>
+
+          <div className='icons'>
+          <Link
+                className={props.active === "contact"}
                 to="/contact"
               >
-                Contact
+                <img src={Search} alt='' />
               </Link>
-            </div>
+
+              <Link
+                className={props.active === "contact"}
+                to="/contact"
+              >
+                <img src={Bag} alt='' />
+              </Link>
           </div>
         </div>
       </nav>
